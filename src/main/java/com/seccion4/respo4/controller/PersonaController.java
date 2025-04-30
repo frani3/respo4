a8f4e42190567ca2247fda3abd1ac05f6cdb046a
frani3 <fra.barrerat@duocuc.cl>
2025-04-30 15:10:08 -0400

    id

--- src/main/java/com/seccion4/respo4/controller/PersonaController.java
+++ src/main/java/com/seccion4/respo4/controller/PersonaController.java
@@ -1,15 +1,22 @@
 package com.seccion4.respo4.controller;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.seccion4.respo4.model.Persona;
 import com.seccion4.respo4.service.PersonaService;
 
+import jakarta.websocket.server.PathParam;
+
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
+
 
 
 
@@ -30,6 +37,18 @@ public class PersonaController {
     @PostMapping
     public Persona postPersona(@RequestBody Persona persona){
         return personaService.guardarPersona(persona);
-    } 
+    }
+    
+    @GetMapping
+    public List<Persona> getPersonas() {
+        return personaService.listarPersonas();
+    }
+    
+    @GetMapping("/{id}")
+    public Persona getPersonaId(@PathVariable int id) {
+        return personaService.buscarxId(id);
+    }
+    
+
     
 }
--- src/main/java/com/seccion4/respo4/repository/PersonaRepository.java
+++ src/main/java/com/seccion4/respo4/repository/PersonaRepository.java
@@ -11,9 +11,35 @@ import com.seccion4.respo4.model.Persona;
 public class PersonaRepository {
     private List<Persona> personas = new ArrayList<>();
 
+    PersonaRepository(){
+        personas.add(new Persona(1, "4564", "Frani", "Kirby", 20));
+        personas.add(new Persona(2, "2345", "Carlos", "Pérez", 30));
+        personas.add(new Persona(3, "6789", "Ana", "Gómez", 25));
+        personas.add(new Persona(4, "1011", "Luis", "Martínez", 40));
+        personas.add(new Persona(5, "1213", "María", "Sánchez", 35));
+    }
+
     public Persona create(Persona persona){
         personas.add(persona);
         System.out.println(personas.toString());
         return persona;
     }
+
+
+    public List<Persona> readAll(){
+        return personas;
+    }
+
+    public Persona read(int id){
+        for (Persona persona : personas){
+            if(persona.getId()==id){
+                return persona;
+            }
+        }
+        return null;
+
+        
+    }
+
+
 }
